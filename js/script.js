f27e62a0654ce9a1db61641916162e61ab78b5cc
milanche7395 <milanche7395@gmail.com>
2022-06-26 22:20:21 +0200

    svašta nešto

--- js/script.js
+++ js/script.js
@@ -115,6 +115,11 @@ $('body').on('click', '.menu', function () {
     $(".navigation").toggleClass("active");
 });
 
+//Social Link
+$('.button-social').click(function () {
+    $(".social-holder").toggleClass('active');
+});
+
 /* Disabled Right Click */
 
 /*$("body").contextmenu(function () {
@@ -148,11 +153,13 @@ function validateForm() {
     var name = document.getElementById('name').value;
     if (name == "") {
         document.querySelector('.status').innerHTML = "Name cannot be empty";
+        $("#name").css('border-color','red');
         return false;
     }
     var email = document.getElementById('email').value;
     if (email == "") {
         document.querySelector('.status').innerHTML = "Email cannot be empty";
+        $("#email").css('border-color','red');
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -164,12 +171,15 @@ function validateForm() {
     var subject = document.getElementById('subject').value;
     if (subject == "") {
         document.querySelector('.status').innerHTML = "Subject cannot be empty";
+        $("#subject").css('border-color','red');
         return false;
     }
     var message = document.getElementById('message').value;
     if (message == "") {
         document.querySelector('.status').innerHTML = "Message cannot be empty";
+        $("#message").css('border-color','red');
         return false;
     }
     document.querySelector('.status').innerHTML = "Sending...";
+    $("#name, #email, #subject, #message").css('border-color','#ced4da');
 }
