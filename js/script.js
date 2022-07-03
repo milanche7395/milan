6dc18eaf47053ce9f90e7cc25f92cf00db7652f5
milanche7395 <milanche7395@gmail.com>
2022-07-03 23:25:47 +0200

    svasta

--- js/script.js
+++ js/script.js
@@ -32,14 +32,6 @@ $(document).ready(function () {
         })
     });
 
-    var $grid = $(".grid").isotope({
-        itemSelector: ".all",
-        percentPosition: true,
-        masonry: {
-            columnWidth: ".all"
-        }
-    })
-
     //   popup 
     $(".gallery").magnificPopup({
         delegate: "a",
@@ -69,25 +61,36 @@ $(document).ready(function () {
     clickNav();
 
     $('#nav').onePageNav({
-       scrollSpeed: 0
+        scrollSpeed: 0
     });
+
+    //Aos Init
+    AOS.init();
 
     //Birthday
     var dob = new Date("12/28/1986");
     //calculate month difference from current date in time
     var month_diff = Date.now() - dob.getTime();
-    
+
     //convert the calculated difference in date format
-    var age_dt = new Date(month_diff); 
-    
+    var age_dt = new Date(month_diff);
+
     //extract year from date    
     var year = age_dt.getUTCFullYear();
-    
+
     //now calculate the age of the user
     var age = Math.abs(year - 1970);
-    
+
     //display the calculated age
     $('.birthday').text(age);
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: true,
+        masonry: {
+            columnWidth: ".all"
+        }
+    })
 
 });
 
@@ -170,13 +173,13 @@ function validateForm() {
     var name = document.getElementById('name').value;
     if (name == "") {
         document.querySelector('.status').innerHTML = "Name cannot be empty";
-        $("#name").css('border-color','red');
+        $("#name").css('border-color', 'red');
         return false;
     }
     var email = document.getElementById('email').value;
     if (email == "") {
         document.querySelector('.status').innerHTML = "Email cannot be empty";
-        $("#email").css('border-color','red');
+        $("#email").css('border-color', 'red');
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -188,15 +191,15 @@ function validateForm() {
     var subject = document.getElementById('subject').value;
     if (subject == "") {
         document.querySelector('.status').innerHTML = "Subject cannot be empty";
-        $("#subject").css('border-color','red');
+        $("#subject").css('border-color', 'red');
         return false;
     }
     var message = document.getElementById('message').value;
     if (message == "") {
         document.querySelector('.status').innerHTML = "Message cannot be empty";
-        $("#message").css('border-color','red');
+        $("#message").css('border-color', 'red');
         return false;
     }
-    document.querySelector('.status').innerHTML = "Sending...";
-    $("#name, #email, #subject, #message").css('border-color','#ced4da');
+    document.querySelector('.status').innerHTML = "Message Sent.";
+    $("#name, #email, #subject, #message").css('border-color', '#ced4da');
 }
